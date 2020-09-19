24cead008dce93c97e49b004d3be21d4133c6907
Julek <31388661+jgrajewsky@users.noreply.github.com>
2020-09-19 17:20:50 +0200

    #beginners

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ require('http').createServer(function (_, res) {
 function start() {
     try {
         const irc_client = new irc.Client("irc.kode.tech", "KhaBridge", {
-            channels: ["#kha", "#kinc"],
+            channels: ["#beginners", "#kha", "#kinc"],
+        });
+
+        irc_client.addListener("message#beginners", function (from, message) {
+            kinc_channel.send(`**<${from}>** ${message}`);
         });
 
         irc_client.addListener("message#kha", function (from, message) {
@@ -25,11 +29,13 @@ function start() {
 
         const discord_client = new discord.Client();
 
+        var beginners_channel;
         var kha_channel;
         var kinc_channel;
 
         discord_client.on("ready", () => {
             discord_client.guilds.fetch("756857638041682015").then(guild => {
+                beginners_channel = guild.channels.cache.get("756859153791713280");
                 kha_channel = guild.channels.cache.get("756857638775423070");
                 kinc_channel = guild.channels.cache.get("756860334790410380");
             });
@@ -37,7 +43,10 @@ function start() {
 
         discord_client.on("message", msg => {
             if (msg.author.id !== "756864665518211203") {
-                if (msg.channel.id === kha_channel.id) {
+                if (msg.channel.id === beginners_channel.id) {
+                    irc_say("#beginners", msg);
+                }
+                else if (msg.channel.id === kha_channel.id) {
                     irc_say("#kha", msg);
                 } else if (msg.channel.id === kinc_channel.id) {
                     irc_say("#kinc", msg);
