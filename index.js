f9df3aed78f0ce13a395586c649dc8d3a9cd9fa4
Julek <31388661+jgrajewsky@users.noreply.github.com>
2020-09-19 16:56:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const http = require('http');
 const irc = require("irc");
 const discord = require("discord.js");
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Hello World!');
+require('http').createServer(function (_, res) {
     res.end();
 }).listen(process.env.PORT);
 
